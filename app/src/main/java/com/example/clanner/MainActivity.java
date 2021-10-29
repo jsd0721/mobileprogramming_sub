d7beb253a2bad884d9181ac929ec077f38731ca7
CASTLE_D_CHO <whtjdehd12@naver.com>
2021-10-29 11:11:23 +0900

    addschedulebutton에 일정추가페이지로 intent 하는기능 할당

--- app/src/main/java/com/example/clanner/MainActivity.java
+++ app/src/main/java/com/example/clanner/MainActivity.java
@@ -2,13 +2,44 @@ package com.example.clanner;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CalendarView;
+
+import com.prolificinteractive.materialcalendarview.MaterialCalendarView;
+
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
+
+    //xml상 컴포넌트랑 연결할 객체 선언
+    Button scheduleAddBtn;
+    MaterialCalendarView calendar;
+
+    //날짜 저장할 문자열 변수 선언
+    String selectedDay;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //객체와 xml상 컴포넌트 연결
+        scheduleAddBtn = (Button)findViewById(R.id.schedule_add_button_mainActivity);
+        calendar = (MaterialCalendarView)findViewById(R.id.calendarView_mainActivity);
+
+
+        selectedDay = String.valueOf(calendar.getCurrentDate());
+
+        scheduleAddBtn.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Intent intent_scheduleAdd = new Intent(getApplicationContext(),add_schedule_Activity.class);
+            startActivity(intent_scheduleAdd);
+
+            }
+        });
     }
 }
