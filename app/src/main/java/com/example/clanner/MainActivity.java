d5dac87533cadaf643dace932639b40f29dcc405
CASTLE_D_CHO <whtjdehd12@naver.com>
2021-11-01 16:05:33 +0900

    로그인 페이지 만듦.

--- app/src/main/java/com/example/clanner/MainActivity.java
+++ app/src/main/java/com/example/clanner/MainActivity.java
@@ -1,23 +1,24 @@
 package com.example.clanner;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.CalendarView;
 
 import com.prolificinteractive.materialcalendarview.CalendarDay;
 import com.prolificinteractive.materialcalendarview.MaterialCalendarView;
 
-import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
 
     //xml상 컴포넌트랑 연결할 객체 선언
     Button scheduleAddBtn;
     MaterialCalendarView calendar;
+    Toolbar tb;
 
     //날짜 저장할 문자열 변수 선언
     String selectedDay;
@@ -30,6 +31,12 @@ public class MainActivity extends AppCompatActivity {
         //객체와 xml상 컴포넌트 연결
         scheduleAddBtn = (Button)findViewById(R.id.schedule_add_button_mainActivity);
         calendar = (MaterialCalendarView)findViewById(R.id.calendarView_mainActivity);
+        tb = (Toolbar)findViewById(R.id.toolbar_mainactivity);
+
+        setSupportActionBar(tb);
+        ActionBar ab = getSupportActionBar();
+        ab.setTitle("");
+
 
 
         selectedDay = String.valueOf(calendar.getCurrentDate());
