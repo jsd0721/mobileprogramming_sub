8920ba29aa8118f94ab460b816e2a477c8c2c7a7
LJIEUN <90252094+LJIEUN@users.noreply.github.com>
2021-11-18 15:01:30 +0900

    프로필에 들어갈 카메라, 갤러리 선택 작업중

---
+++ app/src/main/java/com/example/clanner/GalleryActivity.java
@@ -0,0 +1,35 @@
+package com.example.clanner;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+/*
+public class GalleryActivity extends AppCompatActivity {
+    private RecyclerView recyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager layoutManager;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gallery);
+        recyclerView = findViewById(R.id.recyclerView);
+
+        recyclerView.setHasFixedSize(true);
+
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+
+        String[] myDataset = {"강아지","고양이"};
+
+        mAdapter = new GalleryAdapter(myDataset);
+        recyclerView.setAdapter(mAdapter);
+    }
+}
+
+     */
+
