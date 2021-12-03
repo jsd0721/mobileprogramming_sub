bd5ac8d79328ac4e846f45c2bef8bf5e665937e7
CASTLE_D_CHO <whtjdehd12@naver.com>
2021-12-03 17:14:36 +0900

    인스턴스 생성 횟수 확인해보소

--- app/src/main/java/com/example/clanner/MainActivity.java
+++ app/src/main/java/com/example/clanner/MainActivity.java
@@ -9,6 +9,7 @@ import androidx.appcompat.widget.Toolbar;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.annotation.SuppressLint;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
@@ -49,6 +50,7 @@ public class MainActivity extends AppCompatActivity {
     DatabaseReference DBReference = FBdb.getReference();
     FirebaseUser nowUser = FirebaseAuth.getInstance().getCurrentUser();
     DatabaseReference scheduleReference = DBReference.child("user").child(nowUser.getUid()).child("schedule");
+    DatabaseReference FriendReference = DBReference.child("user").child(nowUser.getUid()).child("Friends");
 
     String CurrentDate;
 
@@ -163,13 +165,13 @@ public class MainActivity extends AppCompatActivity {
         public void onDateSelected(@NonNull MaterialCalendarView widget, @NonNull CalendarDay date, boolean selected) {
             list.clear();
             //선택되어 있는 날짜
-            String Date= String.valueOf(widget.getSelectedDate().getYear()) + "-"
+            String Date_inListener= String.valueOf(widget.getSelectedDate().getYear()) + "-"
                     + String.valueOf(widget.getSelectedDate().getMonth()+1) + "-"
                     + String.valueOf(widget.getSelectedDate().getDay());
-            Log.e("참거짓",String.valueOf(!CurrentDate.equals(Date)));
-            if(!CurrentDate.equals(Date)){
-                CurrentDate = Date;
-                getData(Date);
+            Log.e("참거짓",String.valueOf(!CurrentDate.equals(Date_inListener)));
+            if(!CurrentDate.equals(Date_inListener)){
+                CurrentDate = Date_inListener;
+                getData(CurrentDate);
             }else{
                 Toast.makeText(MainActivity.this, "같은 날짜를 선택하셧습니다.", Toast.LENGTH_SHORT).show();
             }
@@ -177,37 +179,39 @@ public class MainActivity extends AppCompatActivity {
         }
     };
 
-    void getData(String Date){
-        if(scheduleReference.child(Date) != null){
-            scheduleReference.child(Date).addChildEventListener(new ChildEventListener() {
-                @Override
-                public void onChildAdded(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+    <count> void getData(String Date){
+        scheduleReference.child(Date).addChildEventListener(new ChildEventListener() {
+            int count = 0;
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+                scheduleClass scheduleData = snapshot.getValue(scheduleClass.class);
+                Log.d("데이터 들어옴?",scheduleData.content);
+                list.add(scheduleData);
+                RCViewAdapter.notifyDataSetChanged();
+                count++;
+            }
 
-                }
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
 
-                @Override
-                public void onChildChanged(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+            }
 
-                }
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot snapshot) {
 
-                @Override
-                public void onChildRemoved(@NonNull DataSnapshot snapshot) {
+            }
 
-                }
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
 
-                @Override
-                public void onChildMoved(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+            }
 
-                }
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
 
-                @Override
-                public void onCancelled(@NonNull DatabaseError error) {
+            }
+        });
 
-                }
-            });
-        }else{
-
-        }
     }
 
 //    void getData(String Date){
